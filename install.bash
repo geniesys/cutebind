24e27d94beaee20760fde0280f2167d52f2bdb69
geniesys <git@geniesys.net>
2014-11-01 03:28:27 -0700

    Updated
    Will create ./logs and ./ipc empty folders if they don't exist.

--- install.bash
+++ install.bash
@@ -5,6 +5,8 @@ rm /usr/local/bin/cutebind > /dev/null;
 ln -s $CWD/cutebind /usr/local/bin/cutebind
 rm /usr/bin/cutebind  > /dev/null;
 ln -s $CWD/cutebind /usr/bin/cutebind
+mkdir logs 2> /dev/null;
+mkdir ipc  2> /dev/null;
 echo "Installed. Executing cutebind...
 
 ";
